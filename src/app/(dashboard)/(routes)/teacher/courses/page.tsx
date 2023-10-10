8a299d64056dd295b1ec0562b0b739051c91b547
Gustavo Batista <gustavo.ha.batista@gmail.com>
2023-10-10 19:25:30 -0300

    feat: :sparkles: course data table

--- src/app/(dashboard)/(routes)/teacher/courses/page.tsx
+++ src/app/(dashboard)/(routes)/teacher/courses/page.tsx
@@ -1,18 +1,28 @@
-import Link from 'next/link'
+import { auth } from '@clerk/nextjs'
+import { redirect } from 'next/navigation'
 
-import { buttonVariants } from '@/components/ui/button'
+import { columns } from './_components/Columns'
+import { DataTable } from './_components/DataTable'
 
-const CoursesPage = () => {
+import { prisma } from '@/db/prisma'
+
+const CoursesPage = async () => {
+  const { userId } = auth()
+
+  if (!userId) redirect('/')
+
+  const courses = await prisma.course.findMany({
+    where: {
+      userId,
+    },
+    orderBy: {
+      createdAt: 'desc',
+    },
+  })
+
   return (
     <div className="p-6">
-      <Link
-        className={buttonVariants({
-          variant: 'default',
-        })}
-        href="/teacher/create"
-      >
-        New Course
-      </Link>
+      <DataTable columns={columns} data={courses} />
     </div>
   )
 }
